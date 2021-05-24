73694d6419bb6c34ce7c0956db96ea92c6ae1112
koreatech <ziho2463@naver.com>
2021-05-24 21:21:56 +0900

    0524

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	
 //	char *str = "0123456789";
-	int origin = printf("printf    : [%*s]\n",300, "Wow");
-	int ft =  ft_printf("ft_printf : [%*s]\n",300, "Wow");
+//	int origin = printf("printf    : [%s]\n", 123);
+	int ft =  ft_printf("ft_printf : [%s]\n", 123);
 //				 printf("printf    : ABCDE%0*.*dKKKKK\n", 1, 0, 0);
 //			  ft_printf("ft_printf : ABCDE%0*.*dKKKKK\n", 1, 0, 0);
 
-	printf("origin = %d\nft = %d\n", origin, ft);
+//	printf("origin = %d\nft = %d\n", origin, ft);
 //	while(1);
 	return (0);
 }
