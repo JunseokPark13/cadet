ffbf4b7fc5634d8980330c3b999fab637feec75f
koreatech <ziho2463@naver.com>
2021-06-15 00:45:28 +0900

    0615

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,35 +6,76 @@
 /*   By: jupark <jupark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 16:19:24 by jupark            #+#    #+#             */
-/*   Updated: 2021/06/13 17:18:56 by jupark           ###   ########.fr       */
+/*   Updated: 2021/06/15 00:43:06 by jupark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-void	handler(int signo)
+int			g_check;
+
+int			send_signal(pid_t s_pid, char c)
+{
+	int		bit;
+	int		wait;
+	int		retry;
+
+	bit = 0;
+	retry = 0;
+	while (retry++ < 10)
+	{
+		wait = 0;
+		g_check = 0;	
+		if (c & (1 << bit))
+			kill(s_pid, SIGUSR1);
+		else
+			kill(s_pid, SIGUSR2);
+		while (wait++ < 100)
+		{
+			if (g_check)
+			{
+				bit++;
+				retry = 0;
+				break;
+			}
+			usleep(1);
+		}
+		if (bit == 8)
+			return (1);
+	}
+	return (0);
+}
+
+void	sig_handler(int signo)
 {
 	(void)signo;
-	printf("client handler\n");
+	g_check = 1;
 }
 
 int		main(int argc, char *argv[])
 {
 	pid_t	s_pid;
+	int		i;
 	
 	if (argc != 3)
 	{
-		printf("Usage: %s pid string\n", argv[0]);
+		write(1, "Not enough argument!\n", 21);
 		return (1);
 	}
-	signal(SIGUSR1, handler);
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
 	s_pid = atoi(argv[1]);
-	printf("input pid = %d\n", (int)s_pid);
-	printf("SIGUSER1 = %d\n", SIGUSR1);
+	i = 0;
 	while (1)
 	{
-		printf("kill %d\n", kill(s_pid, SIGUSR1));
-		sleep(1);
+		if (!send_signal(s_pid, argv[2][i]))
+		{
+			send_signal(s_pid, '\0');
+			write(1, "There is some Error\n", 20);
+			break;
+		}
+		if (argv[2][i++] == '\0')
+			break;
 	}
-	
+	return (1);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,31 +6,62 @@
 /*   By: jupark <jupark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 16:07:03 by jupark            #+#    #+#             */
-/*   Updated: 2021/06/13 17:18:42 by jupark           ###   ########.fr       */
+/*   Updated: 2021/06/15 00:43:28 by jupark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-void	handler(int signo)
+static void		handler(int signo, siginfo_t *sig)
 {
-	(void)signo;
-	printf("what?\n");
+	static int data;
+	static int bit;
+
+	if (!sig->si_pid || kill(sig->si_pid, signo))
+		return ;
+	if (signo == SIGUSR1)
+		data |= (1 << bit);
+	if (++bit == 8)
+	{
+		if (data)
+			write(1, &data, 1);
+		else
+			write(1, "\n", 1);
+		bit = 0;
+		data = 0;
+	}
+}
+
+static void		ft_putnbr(int n)
+{
+	char	c;
+
+	c = n % 10 + '0';
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	write(1, &c, 1);
 }
 
 int main(void)
 {
-	 pid_t	s_pid;
+	struct sigaction	sigact;
+	pid_t				s_pid;
 
-	 s_pid = getgid();
-	printf("pid = %d\n", getpid());
-	printf("s_pid = %d\n", s_pid);
-//	signal(SIGUSR1, handler);
-	while(1)
+	s_pid = getpid();
+	write(1, "pid = ", 6);
+	ft_putnbr(s_pid);
+	write(1, "\n", 1);
+
+	sigact.sa_flags = SA_SIGINFO;
+	sigact.sa_sigaction = (void *)handler;
+	sigemptyset(&sigact.sa_mask);
+	sigaddset(&sigact.sa_mask, SIGUSR1);
+	sigaddset(&sigact.sa_mask, SIGUSR2);
+	if (sigaction(SIGUSR1, &sigact, NULL) || sigaction(SIGUSR2, &sigact, NULL))
 	{
-		pause();
-//		printf("YES!\n");
-//		sleep(1);
+		printf("error\n");
+		return (1);
 	}
-
+	while(1)
+		pause();
 }
